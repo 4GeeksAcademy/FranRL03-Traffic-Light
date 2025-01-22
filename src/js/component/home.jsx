2d468c0cfd4c4a1021bd1b677c92cb9a6e94393e
Fran Ruiz Lebrón <122262348+FranRL03@users.noreply.github.com>
2025-01-22 16:32:52 +0000

    traffic light done

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+
+	const [selectedColor, setSelectedColor] = useState("red")
+
+	const changeColor = () => {
+		if (selectedColor === "red") {
+		  setSelectedColor("green");
+		} else if (selectedColor === "green") {
+		  setSelectedColor("yellow");
+		} else if (selectedColor === "yellow") {
+		  setSelectedColor("red");
+		}
+	  };
+
+	const purple = () => {
+
+		if (document.querySelector(".purple").classList.contains("light"))
+			document.querySelector(".purple").classList.remove("light")
+		else
+			document.querySelector(".purple").classList.add("light")
+	}
+
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+
+			<h1 className="text-center mt-5">Traffic Light</h1>
+
+			<button type="button" className="btn btn-primary mt-3 mb-3" onClick={purple}>Active/Disable Light Purple</button>
+			<button type="button" className="btn btn-info mt-3 mb-3 ms-3" onClick={changeColor}>Change Color</button>
+
+
+			<div className="traffic-light">
+				<div onClick={() => setSelectedColor('red')} className={"light red" + (selectedColor === 'red' ? ' glow' : '')}></div>
+				<div onClick={() => setSelectedColor('yellow')} className={"light yellow" + (selectedColor === 'yellow' ? ' glow' : '')}></div>
+				<div onClick={() => setSelectedColor('green')} className={"light green" + (selectedColor === 'green' ? ' glow' : '')}></div>
+				<div className="purple"></div>
+			</div>
+
 		</div>
 	);
 };
